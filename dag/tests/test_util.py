b3ef877264e769a7103f8a33f7069aeb03a886fc
Matthew Rocklin <mrocklin@gmail.com>
2012-09-15 09:48:10 -0500

    add reverse_dict util function

--- dag/tests/test_util.py
+++ dag/tests/test_util.py
@@ -10,3 +10,8 @@ def test_merge_many():
     e = {4:5}
     f = {6:7}
     assert merge(d, e, f) == {1:2, 3:4, 4:5, 6:7}
+
+def test_reverse_dict():
+    d = {'a': (1, 2), 'b': (2, 3), 'c':()}
+    assert reverse_dict(d) ==  {1: ('a',), 2: ('a', 'b'), 3: ('b',)}
+
